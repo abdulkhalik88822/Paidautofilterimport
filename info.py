99ed2effb8b1148cd04cd3f89b193b1e2c825eae
Sumit720304 <145983561+Sumit720304@users.noreply.github.com>
2023-11-09 18:36:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,6 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
-RemoveBG_API = environ.get("RemoveBG_API", "")
 
 LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
 
