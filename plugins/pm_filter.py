9c5ba15c152d55e6c375f91cdd906ea09a3e8365
Sumit720304 <145983561+Sumit720304@users.noreply.github.com>
2023-12-06 10:39:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,6 @@ import ast
 import math
 import random
 import pytz
-import os
 from datetime import datetime, timedelta, date, time
 lock = asyncio.Lock()
 
@@ -46,11 +45,12 @@ from plugins.helper.edit_5 import normalglitch_1, normalglitch_2, normalglitch_3
 from info import ADMINS, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, PICS, IMDB, SINGLE_BUTTON, PROTECT_CONTENT, \
     SPELL_CHECK_REPLY, IMDB_TEMPLATE, SHORTLINK_URL, SHORTLINK_API, FIND_CHANNEL
 
+import os
+req_channel = int(environ.get('FIND_CHANNEL', '-1002083512310'))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-req_channel = FIND_CHANNEL
 BUTTON = {}
 BUTTONS = {}
 FRESH = {}
