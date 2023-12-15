6beed0c779190843f7f0ab664d167008a3d90849
Sumit720304 <145983561+Sumit720304@users.noreply.github.com>
2023-12-15 21:20:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,7 +139,7 @@ async def start(client, message):
         for msg in msgs:
             title = msg.get("title")
             size=get_size(int(msg.get("size", 0)))
-            f_caption=msg.get("caption", "")
+            f_caption=msg.get("caption", "{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}")
             if BATCH_FILE_CAPTION:
                 try:
                     f_caption=BATCH_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
