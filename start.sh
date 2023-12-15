07aef3f9280ddb642875c59debfead8f5aaa31da
Sumit720304 <145983561+Sumit720304@users.noreply.github.com>
2023-12-15 10:44:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TG_BOTZ
 fi
-cd /TG_BOTZ 
+cd /TG-Sumit
 pip3 install -U -r requirements.txt
 echo "Starting TG_BOTZ 😎...."
 python3 bot.py    
