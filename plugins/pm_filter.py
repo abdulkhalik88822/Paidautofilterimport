5161adc21fea235807d6b862e9947bc2723eead0
Sumit720304 <145983561+Sumit720304@users.noreply.github.com>
2023-12-20 19:05:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -314,7 +314,7 @@ async def quality_cb_handler(client: Client, query: CallbackQuery):
         0,
         [
             InlineKeyboardButton(
-                text="👇 𝖲𝖾𝗅𝖾𝖼𝗍 𝖸𝗈𝗎𝗋 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾𝗌 👇", callback_data="ident"
+                text="👇 𝖲𝖾𝗅𝖾𝖼𝗍 Quality 👇", callback_data="ident"
             )
         ],
     )
