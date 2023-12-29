0de1d5f764d4048bf93b6e73f5227a623a4e912a
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-12-29 16:16:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/6d754cd447246fe40
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/9133dc596eabc73750774.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6133440326').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5851749250').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001569815531').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1976189133').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
